0cf642fbe5bf722626d1d89a294efa84de786c73
Your Name <nikhilraj13733@gmail.com>
2024-02-03 23:55:01 +0530

    save

---
+++ P7/q.c
@@ -0,0 +1,3 @@
+//ascii value- it stands for americann standard code for information interchange. It is a character encoding format//
+//for text data in computers and on the internet. THe ascii vlaue represents the character variable in numbers and each character variable is//
+//assigned with a value from zero to 127. For example, ASCII value of "A" is 65 "B" 66 and so on
